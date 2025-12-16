37299d7e44de38bd1399d32ddcf8f089441a93ec
EliottB <eliott.buisson@efrei.net>
2025-12-16 11:50:45 +0100

    fix ajouter objet

--- univers/personnage.py
+++ univers/personnage.py
@@ -8,15 +8,22 @@ def initialiser_personnage(nom, prenom, attributs):
     fiche_perso["Attributs"] = attributs
     return fiche_perso
 
+
 def afficher_personnage(personnage):
-    print("Profil du personnage :","\n")
-    for (clé,valeur) in personnage.items():
-        if isinstance(clé,dict):
-            print(clé)
-            for (clé,valeur) in clé.items():
-                print(clé.values())
+    for cle, valeur in personnage.items():
+        if isinstance(valeur, list):
+            print(f"{cle} :")
+            if valeur:
+                for element in valeur:
+                    print(f"- {element}")
+            else:
+                print("[]")
+        elif isinstance(valeur, dict):
+            print(f"{cle} :")
+            for sous_cle, sous_valeur in valeur.items():
+                print(f"- {sous_cle} : {sous_valeur}")
         else:
-            print(clé, "=", valeur)
+            print(f"{cle} : {valeur}")
 
 def modifier_argent(joueur,montant):
     joueur["Galions"] = montant
@@ -28,3 +35,26 @@ def ajouter_objet(joueur,cle,objet):
         joueur["Sortilège"].append(objet)
 
 
+moi = initialiser_personnage("Buisson","Eliott",
+{"courage":8,"intelligence":8,"loyauté":8,"ambition":8})
+
+
+afficher_personnage(moi)
+
+modifier_argent(moi,100000)
+
+ajouter_objet(moi,"Inventaire","LEOBJET")
+
+ajouter_objet(moi, "Sortilège", "LESORT")
+
+
+
+
+
+afficher_personnage(moi)
+
+
+
+
+
+print("test")
